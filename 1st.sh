8e63c5fc61764f7d7b5c67b63285ed3843da7d41
edvin <mikej2279@gmail.com>
2020-08-08 14:27:35 +0200

    ASD

--- 1st.sh
+++ 1st.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#An alternative command for the below command is 'pgrep'. example: pgrep wpa_suppliacnt >> will return the PID only.
+#An alternative command for the below command is 'pgrep'. example: pgrep wpa_suppliacnt >> will return the PID only
 wpa=$(ps aux | grep 'wpa_supp' | head -n 1  | awk '{print $2}') 
 dnsmsq=$(ps aux | grep dnsmasq | head -n 1 | awk '{print $2}')
 kill $wpa
